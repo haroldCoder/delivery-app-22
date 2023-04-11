601c9ebd32f0db84c057af9c0411120815092b9e
haroldCoder <haroldc2005@gmail.com>
2023-04-11 12:54:43 -0500

    reduct

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Home from './components/Home'
 import Help from './components/Help'
 import {Route, BrowserRouter, Routes} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
