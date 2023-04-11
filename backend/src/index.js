cc957afb92530ce5fabaa8c2946415a015eac5b7
haroldCoder <haroldc2005@gmail.com>
2023-04-10 22:39:14 -0500

    mysql2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mysql = require("mysql")
+const mysql = require("mysql2")
 const {
     DB_HOST,
     DB_NAME,
